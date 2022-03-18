0ac30b30186b3d8ea6ddcff6fc3a6c8c81d78dfb
inacionivaldo <97760700+inacionivaldo@users.noreply.github.com>
2022-03-18 15:59:32 -0300

    version 1.0.0
    Primeiro deploy.
    
    Os 'docs' em desenvolvimento foram retirados.
    Os 'docs' que serviam como modelo foram movidos para o diretorio local 'scratch'.

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-#testagem
-#tranformar scripts em funções
+import access_user
+import stations
 
-# import access_user
-from ListaMac import mac_list
-# import stations
-# import neighbor
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import pandas as pd
+import json
+import os
+import re
+
+# es = Elasticsearch(
+#     ['http://192.168.10.14:9200'],
+#     basic_auth=(os.getenv('ELK_USERNAME'), os.getenv('ELK_PASSWORD'))
+# )
+
+df_users = pd.read_csv(os.getenv('dir_users'), delim_whitespace=True)
+df_users.columns = ["UserID", "Username", "IPADDRESS", "MAC", "Status", "DATA", "HORA"]
+
+df_stations = pd.read_csv(os.getenv('dir_stations'), delim_whitespace=True)
+df_stations.columns = ['MAC', 'AP_ID', 'AP_NAME', 'RF/WLAN', 'BAND', 'Type',
+                       'RX/TX', 'RSSI', 'VLAN', 'IPADDRESS', 'SSID', 'DATA', 'HORA']
+
+df_users_stations = pd.merge(df_users, df_stations, on=['MAC'])
+df_users_stations = df_users_stations.drop(columns=['DATA_x', 'HORA_x'])
+
+#metodo para imprimir o dataframe em .csv
+# df_users_stations.to_csv(os.getenv('dir_users_stations'), index=False)
+#%%
+df_users_stations.to_csv(os.getenv('dir_users_stations'), index=False)
 
 
 #%%
+df_users_stations
 
-mac_list
 #%%
