12039c668492a6cbaa8d599523ef1f4f1154dab9
inacionivaldo <97760700+inacionivaldo@users.noreply.github.com>
2022-03-25 13:36:05 -0300

    atualização de script 'teste': junção de funções.

--- teste.py
+++ teste.py
@@ -1,53 +1,50 @@
-#Bibliotecas necessárias
 import time
-import numpy as np
-import pandas as pd
-from io import StringIO
-# from ListaMac import mac_list
-from datetime import datetime
-from dotenv import load_dotenv
-from netmiko import ConnectHandler
-import netmiko
-import re
-import os
+
+from utills import *
+
 load_dotenv()
-
-timestamp_data = datetime.now()
-timestamp = timestamp_data.strftime('%d/%m/%Y %H:%M')
-
-dispositivo = {
-    'device_type': 'huawei',
-    'host': os.getenv('HOST'),
-    'username': os.getenv('USUARIO'),
-    'password': os.getenv('PASSWORD'),
-    'global_delay_factor': 0.1,
-}
 
 start = time.time()
 
-connection = ConnectHandler(**dispositivo)
+connection = ConnectHandler(**AC6005)
 connection.enable()
 
 command = 'display access-user'
+
+outputA = connection.send_command(command)
+outputB = str(outputA)
+output = outputA
+
+#output[303:] essa posição imprime sem o cabeçalho
+print(output)
+
+with open(os.getenv('dir_users'), 'w') as arquivo:
+    output = output.split('\n')
+
+    for valor in output:
+        if re.search('^\s([\d]{1,4})', str(valor)) is None:
+            pass
+        else:
+            arquivo.write(str(valor) + ' ' + timestamp + '\n')
+
+#sleep
+time.sleep(0.30)
+
+command = 'display station all'
+
 outputA = connection.send_command(command)
 outputB = str(outputA)
 output = outputA
 
 connection.disconnect()
 
-output = output[303:]
 print(output)
-type(output)
-#%%
-# # output = output.split('\n')
-# # output.split('\n')
-#%%
-clean = output.replace('--', '')
-clean = clean.replace('                             ', '             ')
-clean = clean.split('\n')
-clean.pop()
-clean.pop()
-clean.remove('')
-clean
 
-#%%
+with open(os.getenv('dir_stations'), 'w') as arquivo:
+    output = output.split('\n')
+
+    for valor in output:
+        if re.search('^([0-9A-Fa-f]{4}[:-])', str(valor)[:6]) is None:
+            pass
+        else:
+            arquivo.write(str(valor) + ' ' + timestamp + '\n')
