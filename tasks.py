784bbbcaa7cf703ae49499d5b1f12306b30890a1
nivaldoinacios <nivaldo.lima@iesb.edu.br>
2022-04-12 18:14:15 -0300

    Version 4.5.0
    Fixed - read_timout error
    Fixed - SSH exception error
    Fixed - redundancy error

--- tasks.py
+++ tasks.py
@@ -1,33 +1,7 @@
 from pkg_rsrcs import *
 
-
-# async def task_1():
-#     exec(open("utils.py").read())
-#
-#
-# async def task_2():
-#     exec(open("etl.py").read())
-#
-#
-# async def task_3():
-#     exec(open("actions.py").read())
-#
-#
-# async def executar_scripts():
-#     print(f'Iniciando scripts {time.strftime("%H:%M:%S")}')
-#     scrip1 = asyncio.create_task(task_1())
-#     script2 = asyncio.create_task(task_2())
-#     # script3 = asyncio.create_task(task_3())
-#     await scrip1
-#     await script2
-#     # await script3
-#     print(f'Finalizando scripts {time.strftime("%H:%M:%S")}')
-#
-#
-# asyncio.run(executar_scripts())
-
 exec(open("utils.py").read())
 
 exec(open("etl.py").read())
 
-exec(open("actions.py").read())
+# exec(open("actions.py").read())
